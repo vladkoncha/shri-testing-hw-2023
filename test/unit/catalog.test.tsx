2bd7b14ba1e36a62537c4a1da8a959d805a62a3d
Vlad <vla.koncha@gmail.com>
2023-06-29 16:13:22 +0700

    adds unit test for product details

--- test/unit/catalog.test.tsx
+++ test/unit/catalog.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen, waitFor } from "@testing-library/react";
 import { CartApi, ExampleApi } from "../../src/client/api";
 import { ApplicationState } from "../../src/client/store";
 import { commerce } from "faker";
@@ -7,7 +8,8 @@ import { Provider } from "react-redux";
 import { Catalog } from "../../src/client/pages/Catalog";
 import { createStore } from "redux";
 import { Product } from "../../src/common/types";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { Application } from "../../src/client/Application";
 
 function createCatalog(initState: ApplicationState) {
   const basename = "/";
@@ -70,26 +72,81 @@ describe("Тестирование каталога", () => {
       const productIdContainer = screen.queryAllByTestId(
         product.id.toString()
       )[0];
-      expect(productIdContainer).toBeDefined();
-      expect(productIdContainer).not.toBeNull();
+      expect(productIdContainer).toBeInTheDocument();
 
       const productName = productIdContainer.querySelector(".ProductItem-Name");
-      expect(productName).not.toBeNull();
-      expect(productName.textContent).toBe(product.name);
+      expect(productName).toBeInTheDocument();
+      expect(productName).toHaveTextContent(product.name);
 
       const productPrice =
         productIdContainer.querySelector(".ProductItem-Price");
-      expect(productPrice).not.toBeNull();
-      expect(productPrice.textContent).toContain(product.price.toString());
+      expect(productPrice).toBeInTheDocument();
+      expect(productPrice).toHaveTextContent(product.price.toString());
 
       const productDetailsLink = productIdContainer.querySelector(
         ".ProductItem-DetailsLink"
       );
-      expect(productDetailsLink).not.toBeNull();
+      expect(productDetailsLink).toBeInTheDocument();
       expect(productDetailsLink).toBeInstanceOf(HTMLAnchorElement);
       expect(productDetailsLink.getAttribute("href")).toBe(
         `/catalog/${product.id}`
       );
     }
   });
+
+  function testProductDescription(
+    product: Product,
+    initState: ApplicationState
+  ) {
+    it(`На странице с подробной информацией продукта ${product.id} отображаются: название товара, его описание, цена, цвет, материал и кнопка "добавить в корзину"`, () => {
+      const store = createStore(() => initState);
+
+      const { container } = render(
+        <MemoryRouter initialEntries={[`/catalog/${product.id}`]}>
+          <Provider store={store}>
+            <Application />
+          </Provider>
+        </MemoryRouter>
+      );
+
+      const productDetailsContainer = container.querySelector(".Product");
+      expect(productDetailsContainer).toBeInTheDocument();
+
+      const testField = (fieldName: string, expectedValue: string) => {
+        const field = productDetailsContainer.querySelector(
+          `.ProductDetails-${fieldName}`
+        );
+        expect(field).toHaveTextContent(expectedValue);
+      };
+
+      const fieldsToTest = [
+        "Name",
+        "Description",
+        "Price",
+        "Color",
+        "Material",
+      ];
+
+      fieldsToTest.forEach((field) => {
+        testField(
+          field,
+          product[field.toLowerCase() as keyof Product].toString()
+        );
+      });
+
+      testField("AddToCart", "Add to Cart");
+    });
+  }
+
+  const products: Product[] = [];
+  initProducts(products);
+  const initState = {
+    details: products,
+    cart: {},
+    products,
+  };
+
+  for (const product of products) {
+    testProductDescription(product, initState);
+  }
 });
