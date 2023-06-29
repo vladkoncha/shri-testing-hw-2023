bf41f6049e7681d17f24c40f534f7ad2aed4c579
Vlad <vla.koncha@gmail.com>
2023-06-29 12:41:56 +0700

    adds hermione screen test for static pages

---
+++ test/hermione/pages.hermione.js
@@ -0,0 +1,24 @@
+let bugId = "";
+if (process.env.BUG_ID !== undefined) {
+  bugId = process.env.BUG_ID;
+}
+
+function getUrl(page) {
+  return "/hw/store/" + page + (bugId && `?bug_id=${bugId}`);
+}
+
+describe("Тестирование страниц", () => {
+  function testAdaptiveWidth(page) {
+    it(`Страница ${page} должна иметь статическое содержимое`, async ({
+      browser,
+    }) => {
+      await browser.setWindowSize(1024, 1000);
+      await browser.url(getUrl(page === "home" ? "" : page));
+      await browser.$(".Application");
+      await browser.assertView(`plain-static-${page}`, ".Application");
+    });
+  }
+
+  const pagesToTest = ["home", "delivery", "contacts"];
+  pagesToTest.forEach((width) => testAdaptiveWidth(width));
+});
